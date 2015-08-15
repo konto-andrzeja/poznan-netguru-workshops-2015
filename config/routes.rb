3e35d4a4b33e5597fb0796cc152bd433ba258eda
ja <e43e43@gmail.com>
2015-08-15 13:48:16 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'reports/subjects', to: 'reports#subjects', as: 'report_subjects'
+
   resources :students do
     get :subjects
   end
+
+  resources :teachers do
+    get :subjects
+  end
+
+  root to: 'visitors#index'
 end
