46e77a8d8d928d1db7b80bd22a03d1b6308d131c
ja <e43e43@gmail.com>
2015-08-15 15:41:35 +0200

    make decorators return expected results

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,7 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_item_notes.for_subject_item(subject_item)
+    "%.2f" % (notes.empty? ? 0 : notes.to_a.sum(&:value).to_f / notes.length)
   end
 end
